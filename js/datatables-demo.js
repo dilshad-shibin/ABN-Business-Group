47110770b192eac6197b751af298fa84536af011
dilshad shibin <dilshadkp05@gmail.com>
2020-07-11 13:16:45 -0700

    some bug changing

---
+++ js/datatables-demo.js
@@ -0,0 +1,15 @@
+// Call the dataTables jQuery plugin-vco
+$(document).ready(function() {
+    $('#dataTable').DataTable();
+});
+
+
+// bruklyn
+$(document).ready(function() {
+    $('#dataTable2').DataTable();
+});
+
+// abn
+$(document).ready(function() {
+    $('#dataTable3').DataTable();
+});
